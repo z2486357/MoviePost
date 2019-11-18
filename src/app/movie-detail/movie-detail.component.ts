57a466edcb80d2f7ea5dd2855896c18de48d8c0d
z2486357 <z2486357@gmail.com>
2019-11-18 13:01:56 -0800

    debug

--- src/app/movie-detail/movie-detail.component.ts
+++ src/app/movie-detail/movie-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { DataService } from '../dataservice.service';
+import { DataService } from '../data-service.service';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
